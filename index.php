f37270c6378ed12ea117d56d28d3b0c045e73058
Angelo <Angelo@DESKTOP-C4PS91T.homenet.telecomitalia.it>
2015-12-26 17:11:28 +0100

    New version: able to parse all the papers (in XML file)

--- index.php
+++ index.php
@@ -8,28 +8,30 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel='stylesheet' type='text/css' href='myCss.css' />
     </head>
     <body>
         <?php
+        
         // put your code here
         $xml=simplexml_load_file("myPubs.xml") or die("Error: Cannot create object");
-        echo "<pre>";
+        //echo "<pre>";
         //print_r($xml);
-        echo "</pre>";
+        //echo "</pre>";
         $lastYear = $xml->publications->publication[0]->year;
         
         echo '<div id = "PublicationList">'
                 . '<div class="year">'
-                . '<p>'.$lastYear.'</p>';
+                . '<p>'.$lastYear.'</p><ul>';
         
         foreach($xml->publications->publication as $publication){
             if(strcmp($lastYear,$publication->year)!=0){
                 $lastYear = $publication->year;
-                echo '</div>'
+                echo '</ul></div>'
                 . '<div class="year">'
-                . '<p>'.$lastYear.'</p>';
+                . '<p>'.$lastYear.'</p><ul>';
             }
-            echo '<div class= "publication">'
+            echo '<li><div class= "publication">'
                 . '<p>'
                 . '<strong>'.implode(", ", (array)$publication->author)
                 //. '('.$publication->year.')'
@@ -44,9 +46,9 @@ and open the template in the editor.
             if(!empty($publication->oro)){echo ' <a href="'.$publication->oro.'" target="_blank"><img src="images/oro.png" alt="ORO Link"/></a>';}
             if(!empty($publication->ee)){echo ' <a href="'.$publication->ee.'" target="_blank"><img src="images/doi.gif" alt="DOI"/></a>';}
             echo '</p>'
-                . '</div>';
+                . '</div></li>';
         }
-        echo '</div>'
+        echo '</ul></div>'
         . '</div>';
         
         //echo $xml->publications->publication[0]->year;
